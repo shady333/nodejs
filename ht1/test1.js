2109dd8f63e64f60c1d14476bf7b60006f56d279
oleg_dudar <oleg_dudar@epam.com>
2019-11-15 14:34:34 +0200

    add some files

---
+++ ht1/test1.js
@@ -0,0 +1,21 @@
+var readline = require('readline')
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout,
+    terminal: false
+});
+
+console.log("Welcome!");
+console.log("Type 'exit' - to stop");
+
+rl.on('line', function(line){
+    if(line == "exit")
+    {
+        process.exit();
+    }
+    else
+    {
+        console.log(line.split("").reverse().join(""));
+    }
+})
