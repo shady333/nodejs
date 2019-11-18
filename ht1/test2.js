193f3aa5469a08e44e0cebd53382708b75370e2e
oleg_dudar <oleg_dudar@epam.com>
2019-11-18 12:02:21 +0200

    Add ht2 sample and supported files

---
+++ ht1/test2.js
@@ -0,0 +1,28 @@
+const csvFilePath='./csv/input_file.csv'
+const csv=require("csvtojson")
+const fs = require('fs')
+const resultFile = 'results.txt'
+
+try {
+    if (fs.existsSync(resultFile)) {
+        console.log('Results file exist. Deleting file ...')
+        fs.truncate(resultFile, 0, (err) => {
+            console.log(err)
+        })
+    }
+  } catch(err) {
+    console.error(err)
+  }
+
+csv()
+.fromFile(csvFilePath)
+.then(function(jsonArrayObj){
+    jsonArrayObj.forEach(function(row) {
+        fs.appendFile(resultFile, JSON.stringify(row)+'\n', (err)=>{
+            if(err)
+                console.log(err)
+        })
+    })
+  }).then(function(){
+      console.log('Completed.')
+  })
