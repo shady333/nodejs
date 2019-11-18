5606b29c4c87b4259d856ee6b8b47b0021587225
oleg_dudar <oleg_dudar@epam.com>
2019-11-18 19:53:28 +0200

    Add babel usage

---
+++ ht1/test1_babel.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// var readline = require('readline');
+
+import readline from 'readline';
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+console.log("Welcome!");
+console.log("Type 'exit' - to stop");
+rl.on('line', function (line) {
+  if (line == "exit") {
+    process.exit();
+  } else {
+    console.log(line.split("").reverse().join(""));
+  }
+});
---
+++ ht1/test2_babel.js
@@ -0,0 +1,28 @@
+"use strict";
+
+import csv from 'csvtojson';
+import fs from 'fs';
+
+var resultFile = 'results_babel.txt';
+var csvFilePath = './csv/input_file.csv';
+
+try {
+  if (fs.existsSync(resultFile)) {
+    console.log('Results file exist. Deleting file ...');
+    fs.truncate(resultFile, 0, function (err) {
+      console.log(err);
+    });
+  }
+} catch (err) {
+  console.error(err);
+}
+
+csv().fromFile(csvFilePath).then(function (jsonArrayObj) {
+  jsonArrayObj.forEach(function (row) {
+    fs.appendFile(resultFile, JSON.stringify(row) + '\n', function (err) {
+      if (err) console.log(err);
+    });
+  });
+}).then(function () {
+  console.log('Completed.');
+});
