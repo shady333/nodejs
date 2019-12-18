6d12bffd24fc2f9357be1dc78aa0dc34eefe6e5a
oleg_dudar <oleg_dudar@epam.com>
2019-12-18 13:32:05 +0200

    Refactoring: Change api methods name

--- ht2/src/init_point.js
+++ ht2/src/init_point.js
@@ -18,16 +18,16 @@ const port = 3000
 let users = []
 
 app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+    return res.send(users);
 })
 
-app.get('/users/:userId', (req, res) => {
+app.get('/find/:userId', (req, res) => {
     const userIndex = getUserPosition(req.params.userId)
     if(userIndex == -1){
         return res.send('Cant find user\n'+req.body)
     }
     else{
-        return res.send(Object.values(users[userIndex]))
+        return res.send(users[userIndex])
     }
 })
 
@@ -48,7 +48,7 @@ app.post('/users', validator.body(querySchema), (req, res) => {
     return res.send("User\n"+Object.values(user)+"\nwas added to the users list")
 })
 
-app.put('/users/:userId', validator.body(querySchema), (req, res) => {
+app.put('/update/:userId', validator.body(querySchema), (req, res) => {
 
     const userIndex = getUserPosition(req.params.userId)
     if(userIndex == -1){
@@ -67,7 +67,7 @@ app.put('/users/:userId', validator.body(querySchema), (req, res) => {
     }
 })
 
-app.delete('/users/:userId', (req, res) => {
+app.delete('/delete/:userId', (req, res) => {
     const userIndex = getUserPosition(req.params.userId)
     if(userIndex == -1){
         return res.send('Cant find user with Id - \n'+req.params.userId)
@@ -76,6 +76,11 @@ app.delete('/users/:userId', (req, res) => {
         users[userIndex].isDeleted = true
         return res.send('PUT request at /users/${req.params.userId}')
     }
+})
+
+app.delete('/destroy', (req, res) => {
+    users = []
+    return res.send('All users were destroyed')
 })
 
 app.listen(port, () => console.log(`My task app listening on port ${port}!`))
@@ -87,3 +92,5 @@ function getAutoSuggestUsers(loginSubString, limit){
 function getUserPosition(id){
     return users.findIndex((element) => element.id == id)
 }
+
+module.exports = app;
