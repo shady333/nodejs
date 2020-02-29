8ce6832949ca149503a2e0940042f72653e24622
Oleg <oleg.dudar@gmail.com>
2020-02-29 17:43:38 +0200

    update script for create and populate users table

--- ht3/create_Users_table.sql
+++ ht3/create_Users_table.sql
@@ -2,8 +2,17 @@ CREATE TABLE public."Users"
 (
     age bigint,
     id bigint NOT NULL,
-    login "char"[],
-    password "char"[],
+    login VARCHAR (255),
+    password VARCHAR (255),
     "isDeleted" boolean,
     CONSTRAINT "Users_pkey" PRIMARY KEY (id)
 )
+
+INSERT INTO public."Users"(id, login, password, age, "isDeleted")
+VALUES
+   (1, 'user1', 'pass1', 10, false),
+   (2, 'user2', 'pass2', 11, false),
+   (3, 'user3', 'pass3', 33, false),
+   (4, 'user3', 'pass3', 70, false),
+   (5, 'user3', 'pass3', 12, true),
+   (6, 'user3', 'pass3', 10, false);
